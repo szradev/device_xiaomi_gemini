781bbf695218765056339f3272944fbb8d29cdfd
szradev <gronkibronka@gmail.com>
2021-02-12 12:55:37 +0100

    Rev

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_gemini.mk
+    $(LOCAL_DIR)/revengeos_gemini.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_gemini-user \
-    aosp_gemini-userdebug \
-    aosp_gemini-eng
+    revengeos_gemini-user \
+    revengeos_gemini-userdebug \
+    revengeos_gemini-eng
--- revengeos_gemini.mk
+++ revengeos_gemini.mk
@@ -19,14 +19,14 @@
 $(call inherit-product, device/xiaomi/gemini/device.mk)
 
 # Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/revengeos/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
 #GApps
 TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_gemini
+PRODUCT_NAME := revengeos_gemini
 PRODUCT_DEVICE := gemini
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 5
